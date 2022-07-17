10e6a3e0a98e6fd280ad2589f0b8e7003670f40c
KevinZzy129 <zhengyaz@andrew.cmu.edu>
2022-07-17 16:34:09 -0700

    Init some files

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class TransactionType(Enum):
+    MEAL = 1
+    TRANSPORTATION = 2
+    ENTERTAINMENT = 3
+    GROCERY = 4
---
+++ src/model.py
@@ -0,0 +1,15 @@
+from typing import List
+from pydantic import BaseModel
+from constants import TransactionType
+from typing import List, Optional
+
+class Transaction(BaseModel):
+    timestamp: int
+    category: TransactionType
+    value: float
+
+class DataItem(BaseModel):
+    date: str
+    transactions: List[Transaction]
+    total_cost: Optional[float]
+    report: Optional[List[float]]
